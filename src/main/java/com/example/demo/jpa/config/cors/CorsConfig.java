29cc97ea82f1e1e18d962828045bfbc73085bf96
PUO <86181292+kl204@users.noreply.github.com>
2024-06-17 16:42:32 +0900

    [update] CORS 해결 중 00 #228

--- src/main/java/com/example/demo/jpa/config/cors/CorsConfig.java
+++ src/main/java/com/example/demo/jpa/config/cors/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://custom.greenseed.or.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
