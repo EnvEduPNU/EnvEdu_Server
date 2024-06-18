8bec6055bf57e3536e6a3061d52456aa14c7090b
PUO <86181292+kl204@users.noreply.github.com>
2024-06-18 16:48:17 +0900

    [update] Post Header Issue 해결 중 14 #228

--- src/main/java/com/example/demo/jpa/user/controller/UserController.java
+++ src/main/java/com/example/demo/jpa/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.jpa.user.controller;
 
+import com.example.demo.jpa.user.dto.request.LoginDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -25,24 +26,14 @@ public class UserController {
             String headerName = headerNames.nextElement();
             String headerValue = request.getHeader(headerName);
             log.info("Header: {} = {}", headerName, headerValue);
-            response.addHeader(headerName, headerValue); // 요청 헤더를 응답 헤더로 복사
+            response.addHeader(headerName, headerValue);
         }
-
-//        String userName = request.getHeader("username");
-//        log.info("username : {}", userName);
-//
-//        String authorization = request.getHeader("authorization");
-//        log.info("authorization : {}", authorization);
-
-
         return ResponseEntity.ok().body("gogoSuccess!!");
     }
 
     @PostMapping("/test")
-    public ResponseEntity<String> postTestMethod(@RequestBody Map<String, Object> body) {
-        if (body.isEmpty()) {
-            return ResponseEntity.badRequest().body("Request body is empty");
-        }
+    public ResponseEntity<String> postTestMethod(@RequestBody LoginDTO body) {
+
         log.info("Request body: {}", body);
         return ResponseEntity.ok("gogoSuccess!!");
     }
