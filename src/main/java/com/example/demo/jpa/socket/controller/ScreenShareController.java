bce561db4e84419c86cae51943ec8e06aa522a20
PUO <86181292+kl204@users.noreply.github.com>
2024-06-19 10:58:00 +0900

    [update] 화면 공유 소켓 통신 이슈: WebRTC 전략  #229

--- src/main/java/com/example/demo/jpa/socket/controller/ScreenShareController.java
+++ src/main/java/com/example/demo/jpa/socket/controller/ScreenShareController.java
@@ -1,15 +1,29 @@
 package com.example.demo.jpa.socket.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class ScreenShareController {
 
-    @MessageMapping("/share")
-    @SendTo("/topic/screen")
-    public String handleScreenShare(String data) {
-        return data;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/sendOffer")
+    public void sendOffer(@Payload String offer) {
+        messagingTemplate.convertAndSend("/topic/offer", offer);
+    }
+
+    @MessageMapping("/sendAnswer")
+    public void sendAnswer(@Payload String answer) {
+        messagingTemplate.convertAndSend("/topic/answer", answer);
+    }
+
+    @MessageMapping("/sendCandidate")
+    public void sendCandidate(@Payload String candidate) {
+        messagingTemplate.convertAndSend("/topic/candidate", candidate);
     }
 }
