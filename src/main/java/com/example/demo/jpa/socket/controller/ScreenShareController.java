810c9f31669aaeadefc61dcac667255c49fd0494
PUO <86181292+kl204@users.noreply.github.com>
2024-06-21 16:57:07 +0900

    [update] 비동기 어노테이션  #229

--- src/main/java/com/example/demo/jpa/socket/controller/ScreenShareController.java
+++ src/main/java/com/example/demo/jpa/socket/controller/ScreenShareController.java
@@ -2,12 +2,11 @@ package com.example.demo.jpa.socket.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-//import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//import org.springframework.scheduling.annotation.Async;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -15,45 +14,28 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class ScreenShareController {
 
-//    private final SimpMessagingTemplate messagingTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
-//    @Async
-//    @MessageMapping("/sendOffer/{sessionId}")
-//    public void sendOffer(@DestinationVariable String sessionId, @Payload String offer) {
-//            messagingTemplate.convertAndSend(String.format("/topic/offer/%s", sessionId), offer);
-//            log.info("Offer sent for session {}: {}", sessionId, offer);
-//    }
-//
-//    @Async
-//    @MessageMapping("/sendAnswer/{sessionId}")
-//    public void sendAnswer(@DestinationVariable String sessionId, @Payload String answer) {
-//            messagingTemplate.convertAndSend(String.format("/topic/answer/%s", sessionId), answer);
-//            log.info("Answer sent for session {}: {}", sessionId, answer);
-//    }
-//
-//    @Async
-//    @MessageMapping("/sendCandidate/{sessionId}")
-//    public void sendCandidate(@DestinationVariable String sessionId, @Payload String candidate) {
-//            messagingTemplate.convertAndSend(String.format("/topic/candidate/%s", sessionId), candidate);
-//            log.info("Candidate sent for session {}: {}", sessionId, candidate);
-//    }
-
-
-    @MessageMapping("/sendOffer")
-    @SendTo("/topic/screen-share")
-    public String sendOffer(@Payload String offer) {
-        return offer;
+    @Async
+    @MessageMapping("/sendOffer/{sessionId}")
+    public void sendOffer(@DestinationVariable String sessionId, @Payload String offer) {
+            messagingTemplate.convertAndSend(String.format("/topic/offer/%s", sessionId), offer);
+            log.info("Offer sent for session {}: {}", sessionId, offer);
     }
 
-    @MessageMapping("/sendAnswer")
-    @SendTo("/topic/screen-share")
-    public String sendAnswer(@Payload String answer) {
-        return answer;
+    @Async
+    @MessageMapping("/sendAnswer/{sessionId}")
+    public void sendAnswer(@DestinationVariable String sessionId, @Payload String answer) {
+            messagingTemplate.convertAndSend(String.format("/topic/answer/%s", sessionId), answer);
+            log.info("Answer sent for session {}: {}", sessionId, answer);
     }
 
-    @MessageMapping("/sendCandidate")
-    @SendTo("/topic/screen-share")
-    public String sendCandidate(@Payload String candidate) {
-        return candidate;
+    @Async
+    @MessageMapping("/sendCandidate/{sessionId}")
+    public void sendCandidate(@DestinationVariable String sessionId, @Payload String candidate) {
+            messagingTemplate.convertAndSend(String.format("/topic/candidate/%s", sessionId), candidate);
+            log.info("Candidate sent for session {}: {}", sessionId, candidate);
     }
+
+
 }
