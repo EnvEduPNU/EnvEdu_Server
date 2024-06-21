3e2421b542740df4013669ce089c26cd3ab65830
PUO <86181292+kl204@users.noreply.github.com>
2024-06-21 15:50:50 +0900

    [update] 비동기 어노테이션  #229

--- src/main/java/com/example/demo/jpa/socket/controller/ScreenShareController.java
+++ src/main/java/com/example/demo/jpa/socket/controller/ScreenShareController.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -15,18 +16,21 @@ public class ScreenShareController {
 
     private final SimpMessagingTemplate messagingTemplate;
 
+    @Async
     @MessageMapping("/sendOffer/{sessionId}")
     public void sendOffer(@DestinationVariable String sessionId, @Payload String offer) {
             messagingTemplate.convertAndSend(String.format("/topic/offer/%s", sessionId), offer);
             log.info("Offer sent for session {}: {}", sessionId, offer);
     }
 
+    @Async
     @MessageMapping("/sendAnswer/{sessionId}")
     public void sendAnswer(@DestinationVariable String sessionId, @Payload String answer) {
             messagingTemplate.convertAndSend(String.format("/topic/answer/%s", sessionId), answer);
             log.info("Answer sent for session {}: {}", sessionId, answer);
     }
 
+    @Async
     @MessageMapping("/sendCandidate/{sessionId}")
     public void sendCandidate(@DestinationVariable String sessionId, @Payload String candidate) {
             messagingTemplate.convertAndSend(String.format("/topic/candidate/%s", sessionId), candidate);
