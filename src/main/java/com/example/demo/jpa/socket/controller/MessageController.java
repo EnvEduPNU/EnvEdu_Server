89fcf4b78c222a9e4dad2f1c98d507edb112700b
PUO <kl204@naver.com>
2024-09-27 12:36:20 +0900

    [update] 화면 공유 소켓 개발 수정

--- src/main/java/com/example/demo/jpa/socket/controller/MessageController.java
+++ src/main/java/com/example/demo/jpa/socket/controller/MessageController.java
@@ -130,7 +130,7 @@ public class MessageController {
 
 
 
-            log.info("과제 공유 상태 : {}", payloadNode.toString());
+            log.info("과제 공유????? 상태 : {}", payloadNode.toString());
 
             // JSON 객체를 문자열로 변환하여 전송
             String jsonPayload = objectMapper.writeValueAsString(payloadNode);
@@ -143,15 +143,15 @@ public class MessageController {
     @MessageMapping("/student-entered")
     private void fromEClassStudenetCheck(@Payload String switchMessage) {
         ObjectMapper objectMapper = new ObjectMapper();
-        boolean screenShared = false;
-        String sessionId = "";
+        boolean entered;
+        String sessionId;
 
         log.info("학생 입장 부분 소켓");
 
         try {
             // JSON 메시지를 파싱하여 값들을 가져옵니다.
             JsonNode rootNode = objectMapper.readTree(switchMessage);
-            screenShared = rootNode.path("screenShared").asBoolean();
+            entered = rootNode.path("entered").asBoolean();
             sessionId = rootNode.path("sessionId").asText();
 
             // 브라우저 창 강제 종료를 대비 false 일시 세션 제거
@@ -161,7 +161,7 @@ public class MessageController {
 
             // JSON 객체를 만들어 그대로 로그에 출력
             ObjectNode payloadNode = objectMapper.createObjectNode();
-            payloadNode.put("screenShared", screenShared);
+            payloadNode.put("entered", entered);
             payloadNode.put("sessionId", sessionId);
 
 
