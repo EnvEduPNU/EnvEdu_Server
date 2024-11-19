67754919546a3a9f6166a47b81509d3bbd3e51ea
PUO <kl204@naver.com>
2024-11-19 21:31:19 +0900

    [update] 공공 데이터 엔티티 요청에 대한 업데이트 06 (오션수정)

--- src/main/java/com/example/demo/jpa/openapi/controller/OpenApiController.java
+++ src/main/java/com/example/demo/jpa/openapi/controller/OpenApiController.java
@@ -132,6 +132,7 @@ public class OpenApiController {
         }
 
         return ResponseEntity.ok(respOcean);
+
     }
 
 
--- src/main/java/com/example/demo/jpa/openapi/service/OpenApiService.java
+++ src/main/java/com/example/demo/jpa/openapi/service/OpenApiService.java
@@ -77,7 +77,7 @@ public class OpenApiService {
 
         log.info("값 검증 : " +oceanQualities.get(0));
 
-        dataChunkService.saveMyDataCompilation(uuid, "OCEANQUALITY", user.get(), now, oceanQualities.size(), memo, title);
+        dataChunkService.saveMyDataCompilation(uuid, "OCEANQUALITY", user.get(), now, oceanQualities.size(), title, memo);
 
         if(openApiRepositoryImpl.saveOceanQuality(oceanQualities)){
             log.info("공공데이터 OceanQuality 저장 완료");
